f2cf80ef4e2355fe686d37800d7a68fefe54a169
MQ MQ <bok.minqian@gmail.com>
2025-07-31 17:29:39 +0800

    Add files via upload

---
+++ JavaScript/build-a-sentence-maker.js
@@ -0,0 +1,19 @@
+
+let adjective = "fuzzy";
+let noun = "dragon";
+let verb = "sneezing";
+let place = "volcano";
+let adjective2 = "glowing";
+let noun2 = "marshmallows";
+let firstStory="Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("First story: "+firstStory);
+adjective="gentle";
+noun="horse";
+verb="running";
+place="quiet village";
+adjective2="wide";
+noun2="apples";
+let secondStory="Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("Second story: "+secondStory);
+
+
---
+++ JavaScript/lab-javascript-trivia-bot.js
@@ -0,0 +1,15 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName="QAQ";
+let botLocation="the universe";
+let favoriteLanguage="JavaScript";
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+console.log("My favorite programming language is "+favoriteLanguage+".");
+let codingFact="NaN is a number in "+favoriteLanguage+"!";
+console.log(codingFact);
+codingFact="0.1 + 0.2 === 0.3 is false in "+favoriteLanguage+"!";
+console.log(codingFact);
+codingFact=favoriteLanguage+" runs in your browser and powers interactive websites!";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
+
+
